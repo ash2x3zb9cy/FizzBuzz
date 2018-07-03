a31caf781985968c31131a31f4525b6c859b43bd
Ash Brent-Carpenter <ash@sent.com>
2018-07-03 15:02:24 +0100

    implement stretch goals!

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// finds the first word beginning with B in an array, and puts a Fezz before it
 function insertFezz(array) {
 	// finds the first index of a word starting with B	
 	let i = array.findIndex(x => x.startsWith('B'));
@@ -7,8 +8,8 @@ function insertFezz(array) {
 	array.splice(i, 0, 'Fezz');
 }
 
-// We now need to specify an order to apply the transformations (see 17)
-const rules = [
+// map of number to transformation. arguments to functions are array of words (eg ['Fizz, 'Buzz'])
+const allRules = [
 	[3, (x) => x.push('Fizz')],
 	[5, (x) => x.push('Buzz')],
 	[7, (x) => x.push('Bang')],
@@ -17,6 +18,16 @@ const rules = [
 	[17, (x) => x.reverse()],
 ];
 
+// builds a set of rules to use from args
+const rules = [];
+let argv = process.argv.slice(2).map(x => parseInt(x, 10));
+for(let i = 0; i < argv.length; i++) {
+	const number = argv[i];
+	let rule = allRules.find(x => x[0] === number);
+	if(rule) rules.push(rule);
+}
+
+// calculates the output for one number and returns it
 function fizzBuzz(number) {
 	const output = [];
 	for(let i = 0; i < rules.length; i++) {
@@ -27,6 +38,22 @@ function fizzBuzz(number) {
 	return output.join('');
 }
 
-for(let i = 1; i <= 255; i++) {
-	console.log(fizzBuzz(i));
+// loops the game from min to max and outputs
+function fizzBuzzLoop(min, max) {
+	for(let i = min; i <= max; i++) {
+		console.log(fizzBuzz(i));
+	}
 }
+
+const rl = require('readline').createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+
+
+// prompts the user for max and runs the game
+rl.question('Choose a maximum number: ', (x) => {
+	fizzBuzzLoop(1, parseInt(x, 10));
+	rl.close();
+});
