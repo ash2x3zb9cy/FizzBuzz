5cbb527a93ad992d541ff2114f3daf479526dae4
Ash Brent-Carpenter <ash@sent.com>
2018-07-03 14:43:10 +0100

    implement part 2

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-for(let i = 1; i <= 100; i++) {
-	let output = '';
-	if (i % 3 === 0) output += 'Fizz';
-	if (i % 5 === 0) output += 'Buzz';
-	if (output.length === 0) output = i.toString();
-	console.log(output);
+function insertFezz(array) {
+	// finds the first index of a word starting with B	
+	let i = array.findIndex(x => x.startsWith('B'));
+	if(i === -1) i = array.length;
+	
+	// insert at specific index
+	array.splice(i, 0, 'Fezz');
 }
 
+// We now need to specify an order to apply the transformations (see 17)
+const rules = [
+	[3, (x) => x.push('Fizz')],
+	[5, (x) => x.push('Buzz')],
+	[7, (x) => x.push('Bang')],
+	[11, (x) => x.splice(0, x.length, 'Bong')],
+	[13, insertFezz],
+	[17, (x) => x.reverse()],
+];
+
+function fizzBuzz(number) {
+	const output = [];
+	for(let i = 0; i < rules.length; i++) {
+		let rule = rules[i];
+		if(number % rules[i][0] === 0) rules[i][1](output);
+	}
+	if(output.length === 0) output.push(number.toString());
+	return output.join('');
+}
+
+for(let i = 1; i <= 255; i++) {
+	console.log(fizzBuzz(i));
+}
